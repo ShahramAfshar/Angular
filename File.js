cae2ba3812fcee6eb4ce4a0e12cc0334242524e6
shahram1990 <shahram.afshar69@gmail.com>
2019-12-10 15:46:27 +0330

    Add Function

---
+++ File.js
@@ -0,0 +1,9 @@
+var Nam = "iman";
+var Nam = "shahram";
+console.log(Nam);
+// let lastName:string="afshar";
+// let lastName:string="shahram"; Error
+{
+    var age = 29;
+}
+console.log(age);
---
+++ File.ts
@@ -0,0 +1,13 @@
+var Nam:string="iman";
+var Nam:string="shahram";
+
+console.log(Nam);
+
+// let lastName:string="afshar";
+// let lastName:string="shahram"; Error
+
+{
+    var age:number=29;
+}
+
+console.log(age);
