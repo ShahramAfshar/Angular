083dd0950dbb1ebb3e228dbf9713a4ac4c91930e
shahram1990 <shahram.afshar69@gmail.com>
2019-12-10 14:00:16 +0330

    variable

---
+++ Test.js
@@ -0,0 +1,8 @@
+var IsOk = true;
+var Number12 = 123;
+var family = "afshar";
+var names = ["ali", "reza"];
+console.log(names);
+var names2 = ["shahram", "hamed"];
+var tuple;
+tuple = ["shahram", 29];
---
+++ Test.ts
@@ -0,0 +1,13 @@
+var IsOk:boolean=true;
+var Number12:number=123;
+var family:string="afshar";
+
+var names:string[]=["ali","reza"];
+console.log(names);
+
+var names2:Array<string>=["shahram","hamed"];
+
+var tuple:[string,number];
+
+tuple=["shahram",29];
+
