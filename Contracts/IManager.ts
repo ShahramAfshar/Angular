89a11ba54b8a2a8f4e0f5151f413a4ce33210a5c
shahram1990 <shahram.afshar69@gmail.com>
2019-12-11 12:10:05 +0330

    FinishTraining

---
+++ Contracts/IManager.ts
@@ -0,0 +1,5 @@
+export interface IManager{
+
+Register(userName:string,email:string):void;
+
+}
---
+++ Services/UserManager.ts
@@ -0,0 +1,10 @@
+import { IManager } from "../Contracts/IManager";
+
+class Manager implements IManager{
+    Register(userName: string, email: string): void {
+       
+        
+    }
+
+
+}
